106ac0dfc0530789b7717a546dec2c95d318bd26
FightingChanceTrades <support@fightingchancetrades.com>
2024-06-28 21:46:03 -0600

    My first Commit

--- crypto.py
+++ crypto.py
@@ -5,14 +5,13 @@ import pandas as pd
 import json, time
 import pytz
 import configparser
-import os
 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 def discord_message(messages):
-    webhook_url = os.getenv('webhook')
+    webhook_url = config.get("ACCOUNTINFO", "webhook")
     if webhook_url == None:
         return
     webhook = SyncWebhook.from_url(url=webhook_url)
@@ -26,8 +25,8 @@ def rh_login(app):
     password: "Your Robinhood password to login"
 
     """
-    user = os.getenv('username')
-    passw = os.getenv('password')
+    user  = config.get("ACCOUNTINFO", "username")
+    passw = config.get("ACCOUNTINFO", "password")
     app.logger.info(f'Username: {user}')
     app.logger.info(f'Password: {passw}')
     login = r.authentication.login(username=user, password=passw, expiresIn=None, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name='')
