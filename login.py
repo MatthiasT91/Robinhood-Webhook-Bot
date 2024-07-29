bfff6cdef58698d9fc51a4065c61e7da033fbf2f
FCT <matthias91.mt@gmail.com>
2024-07-29 09:52:54 -0600

    Update login.py
    Updated Login

--- login.py
+++ login.py
@@ -12,20 +12,16 @@ def read_config(config_path='config.ini'):
     return settings
 
 
-def rh_login(settings):
+def rh_login():
     """
     Robinhood Login:
     username: "Your Username" - Most likely your email
     password: "Your Robinhood password to login"
 
     """
-<<<<<<< HEAD
+    settings = read_config()
     logins = login(username=settings.get('username'), password=settings.get('password'), expiresIn=None, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name='')
 
-=======
-    logins = login(username='your_username', password='your_password', expiresIn=None, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name='')
-    
->>>>>>> e082c8c7b9088abffe69823655f07be220b52239
     if logins.get('access_token'):
         print("Good to go! Logged into Robinhood")
         pass
